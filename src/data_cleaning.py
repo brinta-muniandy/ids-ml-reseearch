01f06ebab8ffb85f4b2a69be719acc2a6f432bea
brinta-muniandy <gs65622@student.upm.edu.my>
2025-07-13 20:19:49 +0800

    Update data_cleaning.py

--- src/data_cleaning.py
+++ src/data_cleaning.py
@@ -1,1 +1,54 @@
-# Placeholder Python script
+import pandas as pd
+import numpy as np
+
+def fix_data_types(df):
+    df = df[df['Dst Port'] != 'Dst Port'].copy()
+
+    int_columns = [ 'Dst Port', 'Protocol', 'Flow Duration', 'Tot Fwd Pkts', 'Tot Bwd Pkts',
+        'TotLen Fwd Pkts', 'TotLen Bwd Pkts', 'Fwd Pkt Len Max', 'Fwd Pkt Len Min',
+        'Bwd Pkt Len Max', 'Bwd Pkt Len Min', 'Flow IAT Max', 'Flow IAT Min',
+        'Fwd IAT Tot', 'Fwd IAT Max', 'Fwd IAT Min', 'Bwd IAT Tot', 'Bwd IAT Max',
+        'Bwd IAT Min', 'Fwd PSH Flags', 'Bwd PSH Flags', 'Fwd URG Flags',
+        'Bwd URG Flags', 'Fwd Header Len', 'Bwd Header Len', 'Pkt Len Min',
+        'Pkt Len Max', 'FIN Flag Cnt', 'SYN Flag Cnt', 'RST Flag Cnt',
+        'PSH Flag Cnt', 'ACK Flag Cnt', 'URG Flag Cnt', 'CWE Flag Count',
+        'ECE Flag Cnt', 'Down/Up Ratio', 'Fwd Byts/b Avg', 'Fwd Pkts/b Avg',
+        'Fwd Blk Rate Avg', 'Bwd Byts/b Avg', 'Bwd Pkts/b Avg', 'Bwd Blk Rate Avg',
+        'Subflow Fwd Pkts', 'Subflow Fwd Byts', 'Subflow Bwd Pkts', 'Subflow Bwd Byts',
+        'Init Fwd Win Byts', 'Init Bwd Win Byts', 'Fwd Act Data Pkts',
+        'Fwd Seg Size Min', 'Active Max', 'Active Min', 'Idle Max', 'Idle Min'
+    ]
+
+    float_columns = [
+        'Fwd Pkt Len Mean', 'Fwd Pkt Len Std', 'Bwd Pkt Len Mean', 'Bwd Pkt Len Std',
+        'Flow Byts/s', 'Flow Pkts/s', 'Flow IAT Mean', 'Flow IAT Std',
+        'Fwd IAT Mean', 'Fwd IAT Std', 'Bwd IAT Mean', 'Bwd IAT Std',
+        'Fwd Pkts/s', 'Bwd Pkts/s', 'Pkt Len Mean', 'Pkt Len Std', 'Pkt Len Var',
+        'Pkt Size Avg', 'Fwd Seg Size Avg', 'Bwd Seg Size Avg',
+        'Active Mean', 'Active Std', 'Idle Mean', 'Idle Std'
+    ]
+
+    for col in int_columns:
+        if col in df.columns:
+            df[col] = pd.to_numeric(df[col], errors='coerce').fillna(0).astype(int)
+
+    for col in float_columns:
+        if col in df.columns:
+            df[col] = pd.to_numeric(df[col], errors='coerce').astype(float)
+
+    return df
+
+def drop_inf_and_null(df):
+    df = df.replace(["Infinity", "infinity"], np.inf)
+    df = df.replace([np.inf, -np.inf], np.nan)
+    df.dropna(inplace=True)
+    return df
+
+def drop_unnecessary_columns(df):
+    if 'Timestamp' in df.columns:
+        df.drop(columns=['Timestamp'], inplace=True)
+    return df
+
+def transform_label_to_binary(df):
+    df['Label'] = df['Label'].apply(lambda x: "Benign" if x == 'Benign' else "Malicious")
+    return df
