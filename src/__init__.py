6efd296f70ca45fccd6b4a360f1f83e31623751d
brinta-muniandy <gs65622@student.upm.edu.my>
2025-07-13 20:17:51 +0800

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,1 +1,10 @@
-# Placeholder Python script
+# src/__init__.py
+
+# This file marks the directory as a Python package.
+# You can use it to expose selected functionality at the package level.
+
+from . import data_cleaning
+from . import eda
+from . import feature_selection
+from . import model_training
+from . import utils
