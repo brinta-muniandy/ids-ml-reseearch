97b054bb27f1fc70655c712d7cb71fa69e74eeef
brinta-muniandy <gs65622@student.upm.edu.my>
2025-07-13 20:18:13 +0800

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -1,1 +1,9 @@
-# Placeholder Python script
+from imblearn.under_sampling import RandomUnderSampler
+import pandas as pd
+
+def balance_data(df):
+    X = df.drop("Label", axis=1)
+    y = df["Label"]
+    rus = RandomUnderSampler()
+    X_bal, y_bal = rus.fit_resample(X, y)
+    return pd.concat([pd.DataFrame(X_bal, columns=X.columns), pd.DataFrame(y_bal, columns=["Label"])], axis=1)
